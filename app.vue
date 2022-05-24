8b678c46d7b9807e45b933564faf3ebd950b92ea
Adam Gregořica <44508744+Gregstr05@users.noreply.github.com>
2022-05-24 22:03:38 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">  
+    <Rating :grade="3" :maxStars="5" :hasCounter="true" />
+  </div>
+</template>
+
+<script>
+import Rating from './components/Rating.vue'
+export default {
+  name: 'App',
+  components: {
+    Rating
+  },
+  beforeCreate: function() {
+    document.body.className = 'home';
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '/styles/global.scss';
+</style>
