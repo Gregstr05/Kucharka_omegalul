41d57dce98faa174133495405e733b2f91131aa1
gregstr05 <agregorica@gmail.com>
2022-06-05 14:41:03 +0200

    added index.js and tried swithcing between dark and white theme

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-import Vue from 'vue'
+/*import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app')*/
+
+function isLight() {
+  return localStorage.getItem("light-mode");
+}
+
+function toggleRootClass() {
+  document.querySelector(":root").classList.toggle("light");
+}
+
+function toggleLocalStorageItem() {
+  if (isLight()) {
+    localStorage.removeItem("light-mode");
+  } else {
+    localStorage.setItem("light-mode", "set");
+  }
+}
+
+if (isLight()) {
+  toggleRootClass();
+}
+
+document.querySelector("#Dark_Theme").addEventListener("click", () => {
+  toggleLocalStorageItem();
+  toggleRootClass();
+});
